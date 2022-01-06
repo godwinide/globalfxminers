4f37382ff4b4c27861e53cba7d521db40e824975
Godwin Idemudia <godwinide@gmail.com>
2022-01-06 12:53:15 +0100

    new'

--- config/auth.js
+++ config/auth.js
@@ -6,12 +6,6 @@ module.exports = {
       req.flash('error_msg', 'Login required');
       res.redirect('/trade/signin');
     },
-    ensureIsAdmin: function (req, res, next) {
-      if (!(req.isAuthenticated && req.user?.isAdmin == true)) {
-          return next()
-      }
-      res.redirect("/admin/signin");
-    },
     forwardAuthenticated: function(req, res, next) {
       if (req.isAuthenticated()) {
         return next();
